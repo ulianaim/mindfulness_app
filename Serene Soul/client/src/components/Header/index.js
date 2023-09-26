ada8e941d9fcccf0365d93fc9ec4912991c1c014
Lexi Geller <leximoconner@yahoo.com>
2023-09-25 20:05:18 -0400

    added logo to header>index.js

--- Serene Soul/client/src/components/Header/index.js
+++ Serene Soul/client/src/components/Header/index.js
@@ -7,39 +7,43 @@ const Header = () => {
     event.preventDefault();
     Auth.logout();
   };
-  
-return (
- 
-  <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-  <div className="container flex-row justify-space-between-lg justify-center align-center">
-    <div>
-      <Link className="text-light" to="/">
-        <h1 className="m-0">SereneSoul</h1>
-      </Link>
-      <p className="m-0">Acknowledge, accept, and honour that you deserve your own deepest compassion and love</p>
-    </div>
-    <div>
-      {Auth.loggedIn() ? (
-        <>
-          <span>Welcome {Auth.getProfile().data.username}!</span>
-          <button className="btn btn-lg btn-light m-2" onClick={logout}>
-            Logout
-          </button>
-        </>
-      ) : (
-        <>
-          <Link className="btn btn-lg btn-info m-2" to="/login">
-            Login
+
+  return (
+    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <div>
+          <Link className="text-light" to="/">
+            {}
+            <img
+              src="/logo.svg"
+              alt="SereneSoul Logo"
+              height="40"
+            />
           </Link>
-          <Link className="btn btn-lg btn-light m-2" to="/signup">
-            Signup
-          </Link>
-        </>
-      )}
-    </div>
-  </div>
-</header>
-)
-}
+          <p className="m-0">Acknowledge, accept, and honor that you deserve your own deepest compassion and love</p>
+        </div>
+        <div>
+          {Auth.loggedIn() ? (
+            <>
+              <span>Welcome {Auth.getProfile().data.username}!</span>
+              <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link className="btn btn-lg btn-info m-2" to="/login">
+                Login
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/signup">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
