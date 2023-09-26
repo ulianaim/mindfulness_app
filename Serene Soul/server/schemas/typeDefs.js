f270f5991ea28337a2a9d440f550dbab529ecdce
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-25 20:04:18 -0400

    graphql working

--- Serene Soul/server/schemas/typeDefs.js
+++ Serene Soul/server/schemas/typeDefs.js
@@ -27,6 +27,7 @@ const typeDefs = gql`
   }
 
   type Mutation {    
+    login(email: String!, password: String!): Auth
     addQuote(quoteId: ID!, quoteText: String!): Quote
     removeQuote(quoteId: ID!): Quote
     updateQuote(quoteId: ID!, quoteText: String!): Quote
